90ae32963269d253695a541921a17140c1f3907e
DJDuong <36392909+DJDuong@users.noreply.github.com>
2018-05-14 15:52:10 +1000

    3 - Class corrected, EStop variable added, All global.
    Corrected initialisation stage (class instantiation), added emergency stop boolean variable 'stopped'. currently passing by global variables.

---
+++ snl_gui_rev2.m
@@ -0,0 +1,164 @@
+function varargout = snl_gui(varargin)
+% SNL_GUI MATLAB code for snl_gui.fig
+%      SNL_GUI, by itself, creates a new SNL_GUI or raises the existing
+%      singleton*.
+%
+%      H = SNL_GUI returns the handle to a new SNL_GUI or the handle to
+%      the existing singleton*.
+%
+%      SNL_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SNL_GUI.M with the given input arguments.
+%
+%      SNL_GUI('Property','Value',...) creates a new SNL_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before snl_gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to snl_gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help snl_gui
+
+% Last Modified by GUIDE v2.5 13-May-2018 17:26:33
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @snl_gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @snl_gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before snl_gui is made visible.
+function snl_gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to snl_gui (see VARARGIN)
+% ====================BELOW IS RUN BEFORE GUI APPEARS=====================%
+
+    %> global variables where some have default values; initialised before
+    % the gui is made visible
+    global robot;
+    global realistic;
+    global fps;
+    global scale;
+    global steps;
+    global s;
+    global qZero1;
+    global stopped;
+    
+    stopped = false;
+    
+    % realistic variable = 1 - realistic arms, 2 - simple shape arms, 3 - 
+    % stick model arms.
+    realistic = 3;          
+    
+    % handle for objects cyton class
+    robot = Cyton();         
+    
+    % fps variable that sets the frame rate of the arm plot
+    fps = 25;       
+    
+    % scale of the stick and simple shapes models, kept low to ensure the
+    % realistic plot can be seen
+    scale = 0.3;            
+    
+    % steps variable for number of steps
+    steps = 25;          
+    
+    % trapezoidal trajectory interpolation technique
+    s = lspb(0,1,steps);    
+    
+    % simple zero joint state for non actuated state.
+    qZero1 = [0 0 0 0 0 0 0];                                                                                          
+    
+% ====================ABOVE IS RUN BEFORE GUI APPEARS=====================%
+% Choose default command line output for snl_gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes snl_gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = snl_gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in EmergencyStop.
+function EmergencyStop_Callback(hObject, eventdata, handles)
+% hObject    handle to EmergencyStop (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global stopped
+stopped = true;
+% Hint: get(hObject,'Value') returns toggle state of EmergencyStop
+
+
+% --- Executes on button press in Move.
+function Move_Callback(hObject, eventdata, handles)
+% hObject    handle to Move (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in RandomDie.
+function RandomDie_Callback(hObject, eventdata, handles)
+% hObject    handle to RandomDie (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function ManualDie_Callback(hObject, eventdata, handles)
+% hObject    handle to ManualDie (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of ManualDie as text
+%        str2double(get(hObject,'String')) returns contents of ManualDie as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function ManualDie_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ManualDie (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in SenseDie.
+function SenseDie_Callback(hObject, eventdata, handles)
+% hObject    handle to SenseDie (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
