4498ed7e31750aed00feeaede6ad96d301f3ce3e
Firepoker <scott.a.clarkson@student.uts.edu.au>
2018-05-22 17:55:48 +1000

    updated initial guess

--- RMRS.m
+++ RMRS.m
@@ -1,6 +1,6 @@
-function qMatrix = RMRS(cyton, startPos, endPos, rpy)
+function qMatrix = RMRS(cyton, startPos, endPos, startRPY, endRPY)
     t = 3;             % Total time (s)
-    deltaT = 0.02;      % Control frequency
+    deltaT = 0.05;      % Control frequency
     steps = t/deltaT;   % No. of steps for simulation
     delta = 2*pi/steps; % Small angle change
     epsilon = 0.1;      % Threshold value for manipulability/Damped Least Squares
@@ -13,7 +13,7 @@ function qMatrix = RMRS(cyton, startPos, endPos, rpy)
     x = zeros(3,steps);             % Array for x-y-z trajectory
     positionError = zeros(3,steps); % For plotting trajectory error
     angleError = zeros(3,steps);    % For plotting trajectory error
-
+    
     s = lspb(0,1,steps);             % Trapezoidal trajectory scalar
     for i=1:steps
         x(1,i) = (1-s(i))*startPos(1) + s(i)*endPos(1);     % Points in x
@@ -24,38 +24,38 @@ function qMatrix = RMRS(cyton, startPos, endPos, rpy)
         theta(3,i) = (1-s(i))*startRPY(3) + s(i)*endRPY(3); % Yaw angle
     end
  
-    T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];          % Create transformation of first point and angle
-    q0 = zeros(1,7);                                                            % Initial guess for joint angles
-    qMatrix(1,:) = cyton.ikcon(T,q0);                                           % Solve joint angles to achieve first waypoint
+    T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];     % Create transformation of first point and angle
+    q0 = [-1 0.5 1 1 0 0 -pi/2];                                             % Initial guess for joint angles
+    qMatrix(1,:) = cyton.ikcon(T,q0);                                      % Solve joint angles to achieve first waypoint
 
     for i = 1:steps-1
-        T = cyton.fkine(qMatrix(i,:));                                          % Get forward transformation at current joint state
-        deltaX = x(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
-        Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
-        Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
-        Rdot = (1/deltaT)*(Rd - Ra);                                            % Calculate rotation matrix error
-        S = Rdot*Ra';                                                           % Skew symmetric!
+        T = cyton.fkine(qMatrix(i,:));                                     % Get forward transformation at current joint state
+        deltaX = x(:,i+1) - T(1:3,4);                                      % Get position error from next waypoint
+        Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                % Get next RPY angles, convert to rotation matrix
+        Ra = T(1:3,1:3);                                                   % Current end-effector rotation matrix
+        Rdot = (1/deltaT)*(Rd - Ra);                                       % Calculate rotation matrix error
+        S = Rdot*Ra';                                                      % Skew symmetric!
         linear_velocity = (1/deltaT)*deltaX;
-        angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
-        deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
-        xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
-        J = cyton.jacob0(qMatrix(i,:));                                         % Get Jacobian at current joint state
+        angular_velocity = [S(3,2);S(1,3);S(2,1)];                         % Check the structure of Skew Symmetric matrix!!
+        deltaTheta = tr2rpy(Rd*Ra');                                       % Convert rotation matrix to RPY angles
+        xdot = W*[linear_velocity;angular_velocity];                       % Calculate end-effector velocity to reach next waypoint.
+        J = cyton.jacob0(qMatrix(i,:));                                    % Get Jacobian at current joint state
         m(i) = sqrt(det(J*J'));
-        if m(i) < epsilon                                                       % If manipulability is less than given threshold
+        if m(i) < epsilon                                                  % If manipulability is less than given threshold
             lambda = (1 - m(i)/epsilon)*5E-2;
         else
             lambda = 0;
         end
-        invJ = inv(J'*J + lambda *eye(7))*J';                                   % DLS Inverse
-        qdot(i,:) = (invJ*xdot)';                                               % Solve the RMRC equation (you may need to transpose the vector)
-        for j = 1:7                                                             % Loop through joints 1 to 7
-            if qMatrix(i,j) + deltaT*qdot(i,j) < cyton.qlim(j,1)                % If next joint angle is lower than joint limit...
+        invJ = inv(J'*J + lambda *eye(7))*J';                              % DLS Inverse
+        qdot(i,:) = (invJ*xdot)';                                          % Solve the RMRC equation (you may need to transpose the vector)
+        for j = 1:7                                                        % Loop through joints 1 to 7
+            if qMatrix(i,j) + deltaT*qdot(i,j) < cyton.qlim(j,1)           % If next joint angle is lower than joint limit...
                 qdot(i,j) = 0; % Stop the motor
-            elseif qMatrix(i,j) + deltaT*qdot(i,j) > cyton.qlim(j,2)            % If next joint angle is greater than joint limit ...
+            elseif qMatrix(i,j) + deltaT*qdot(i,j) > cyton.qlim(j,2)       % If next joint angle is greater than joint limit ...
                 qdot(i,j) = 0; % Stop the motor
             end
         end
-        qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                       % Update next joint state based on joint velocities
+        qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                  % Update next joint state based on joint velocities
     end
     plot3(x(1,:),x(2,:),x(3,:),'k.','LineWidth',1)
 end
