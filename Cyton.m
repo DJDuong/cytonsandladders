d91d6e6292f85ccd3ea9f22d6ec5bf0e8e968977
Firepoker <scott.a.clarkson@student.uts.edu.au>
2018-05-22 17:58:05 +1000

    fixed DH parameters.....again

--- Cyton.m
+++ Cyton.m
@@ -8,22 +8,23 @@ classdef Cyton < handle
         function self = Cyton()
         self.GetCytonRobot();
         end
+
 %% GetCytonRobot
         function GetCytonRobot(self)
         pause(0.001);
         name = ['Cyton_',datestr(now,'yyyymmddTHHMMSSFFF')];
 
-        L1 = Link('d',0.12,'a',0,'alpha',pi/2,'offset', -pi/2,'qlim',[deg2rad(-150),deg2rad(150)]);
+        L1 = Link('d',0.12,'a',0,'alpha',pi/2,'offset', 0,'qlim',[deg2rad(-150),deg2rad(150)]);
         L2 = Link('d',0,'a',0,'alpha',-pi/2,'offset', 0,'qlim',[deg2rad(-105),deg2rad(105)]);
         L3 = Link('d',0.1408,'a',0,'alpha',pi/2,'offset', 0,'qlim',[deg2rad(-150),deg2rad(150)]);
-        L4 = Link('d',0,'a',0.0718,'alpha',-pi/2,'offset', pi/2,'qlim',[deg2rad(-105),deg2rad(105)]);
+        L4 = Link('d',0,'a',0.0718,'alpha',pi/2,'offset', pi/2,'qlim',[deg2rad(-105),deg2rad(105)]);
         L5 = Link('d',0,'a',0.1296,'alpha',pi/2,'offset', 0,'qlim',[deg2rad(-105),deg2rad(105)]);
         L6 = Link('d',0,'a',0,'alpha',-pi/2,'offset', -pi/2,'qlim',[deg2rad(-105),deg2rad(105)]);
-        L7 = Link('d',0,'a',0,'alpha',0,'offset', -pi/2,'qlim',[deg2rad(-150),deg2rad(150)]);
+        L7 = Link('d',0,'a',0,'alpha',0,'offset', pi,'qlim',[deg2rad(-150),deg2rad(150)]);
 
         self.model = SerialLink([L1 L2 L3 L4 L5 L6 L7],'name',name);
         end
-        %% PlotAndColourRobot
+%% PlotAndColourRobot
         % Given a robot index, add the glyphs (vertices and faces) and
         % colour them in if data is available 
         function PlotAndColourRobot(self)
