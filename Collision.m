9815bcb74f4e441e091fa3b565735519943615b9
Firepoker <scott.a.clarkson@student.uts.edu.au>
2018-05-27 18:12:27 +1000

    accepts joint matrix and object
    has placeholder methods to extract object information

--- Collision.m
+++ Collision.m
@@ -1,32 +1,32 @@
-function isCollision = EndCheckCollisionCartesion(cyton, jointCond, minXYZ, maxXYZ)
-    cytonPos = cyton.fkine(jointCond);
-    cytonPos = cytonPos(1:3, 4)';
-    isCollision = false;
-    %checks if end effector is within bounds of a rectangular object
-    if (cytonPos(1) >= minXYZ(1) && cytonPos(1) <= maxXYZ(1) ...
-     && cytonPos(2) >= minXYZ(2) && cytonPos(2) <= maxXYZ(2) ...
-     && cytonPos(3) >= minXYZ(3) && cytonPos(3) <= maxXYZ(3))
-        isCollision = true;
-    end
-end
-
-function isCollision = CheckCollisionCartesion(cyton, jointCond, minXYZ, maxXYZ)
+function isCollision = CheckCollision(cyton, jointMatrix, object)
     isCollision = false;
     tr = zeros(4,4,cyton.n+1);
     tr(:,:,1) = cyton.base;
     L = cyton.links;
+    
+    %get object center point and radius
+    objectPoint = object.point;
+    radius = object.radius;
+    %make sure work
+    
     %gets translations for each link
-    for i = 1 : cyton.n
-        tr(:,:,i+1) = tr(:,:,i) * trotz(jointCond(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
-    end
-    for i = 1 : size(tr,3)-1
-        %extracts xyz of each translation
-        coodPos = tr(1:3, 4, i)';
-        %checks if any links are within bounds of a rectangular object
-        if (coodPos(1) >= minXYZ(1) && coodPos(1) <= maxXYZ(1) ...
-        && coodPos(2) >= minXYZ(2) && coodPos(2) <= maxXYZ(2) ...
-        && coodPos(3) >= minXYZ(3) && coodPos(3) <= maxXYZ(3))
-            isCollision = true;
+    for j = 1 : size(jointMatrix, 1)
+        jointCond = jointMatrix(j, :);
+        for i = 1 : cyton.n
+            tr(:,:,i+1) = tr(:,:,i) * trotz(jointCond(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+        end
+        for i = 1 : size(tr,3)-1
+            %extracts xyz of each translation
+            jointPos = tr(1:3, 4, i)';
+            %checks if any links are within bounds of object
+            if (radius <= DistanceBetweenPoints(jointPos, objectPoint))
+                isCollision = true;
+            end
         end
     end
+end
+function distance = DistanceBetweenPoints(point1, point2)
+    distance = sqrt((pt1(1)-pt2(1)).^2+...
+                    (pt1(2)-pt2(2)).^2+...
+                    (pt1(3)-pt2(3)).^2);
 end
